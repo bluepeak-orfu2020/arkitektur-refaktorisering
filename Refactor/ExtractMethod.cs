9c66e48926ead8bf144f1758ddb1b3b2a9ae34e8
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-06-02 09:41:54 +0200

    Add ExtractMethod example

---
+++ Refactor/ExtractMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactor
+{
+    class ExtractMethod
+    {
+        public void Compute(int age)
+        {
+            List<string> users = new List<string>();
+
+            foreach (string user in users)
+            {
+                Console.Write($"Test: {user} {age}");
+            }
+        }
+    }
+}
